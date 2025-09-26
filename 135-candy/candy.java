60bbf30b9288ccc6e250087dec1ee7f127345e24
ishant-jaiswal <ishantjaiswal2004@gmail.com>
2025-09-27 02:43:35 +0530

    Time: 3 ms (80.35%) | Memory: 45.8 MB (57.75%) - LeetSync

---
+++ 135-candy/candy.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int candy(int[] ratings) {
+         int n = ratings.length;
+        int[] candies = new int[n];
+        Arrays.fill(candies, 1); // step 1: everyone gets 1 candy
+
+        // left to right
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        // right to left
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+
+        // sum all candies
+        int total = 0;
+        for (int c : candies) total += c;
+        return total;
+    }
+}
